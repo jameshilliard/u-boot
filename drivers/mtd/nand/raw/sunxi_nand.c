527867eacdcafbc0549b8c9b53ea1a376d154e14
Richard Genoud <richard.genoud@bootlin.com>
2025-09-18 17:02:45 +0200

    sunxi_nand: add missing clocks ?
    Signed-off-by: Richard Genoud <richard.genoud@bootlin.com>

--- drivers/mtd/nand/raw/sunxi_nand.c
+++ drivers/mtd/nand/raw/sunxi_nand.c
@@ -196,8 +196,12 @@ static void sunxi_nfc_set_clk_rate(unsigned long hz)
 
 	/* gate on nand clock */
 #if defined (CONFIG_MACH_SUN50I_H616) || defined (CONFIG_MACH_SUN50I_H6)
-	setbits_le32(ccm + CCU_H6_NAND_GATE_RESET, (1 << GATE_SHIFT));
+	setbits_le32(ccm + CCU_H6_NAND_GATE_RESET,
+		     (1 << GATE_SHIFT) | (1 << RESET_SHIFT));
 	setbits_le32(ccm + CCU_H6_MBUS_GATE, (1 << MBUS_GATE_OFFSET_NAND));
+	writel(CCM_NAND_CTRL_ENABLE | CCM_NAND_CTRL_PLL6 |
+	       CCM_NAND_CTRL_N(div_n) | CCM_NAND_CTRL_M(div_m),
+	       ccm + CCU_NAND1_CLK_CFG);
 #else
 	setbits_le32(&ccm->ahb_gate0, (1 << AHB_GATE_OFFSET_NAND0));
 #ifdef CONFIG_MACH_SUN9I
